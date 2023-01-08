127930c579b28e33958825e0af550a135511fee9
UroojIslamKhan <121480546+Urooj-Islam@users.noreply.github.com>
2023-01-08 13:22:11 +0500

    Add files via upload

---
+++ If statements in cpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std;
+int main() 
+{
+int a;
+cout << "Enter a number: ";
+cin >> a; //get number
+if (a == 10)
+cout << "The number is equal to 10";
+if (a > 10)
+cout << "The number is greater than 10";
+if (a < 10)
+cout << "The number is less than 10";
+return 0;
+}
